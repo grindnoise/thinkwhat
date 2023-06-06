d3008fa92f920439e9e0a0f6b9fa210d4a3416ab
Pavel <pbuxaroff@gmail.com>
2023-06-06 18:46:33 +0300

    testing

--- ThinkWhat/Modules/Controllers/App/Hot/HotController.swift
+++ ThinkWhat/Modules/Controllers/App/Hot/HotController.swift
@@ -318,7 +318,7 @@ extension HotController: DataObservable {
       navigationController?.pushViewController(PollController(surveyReference: SurveyReferences.shared.all.first!), animated: true)
       tabBarController?.setTabBarVisible(visible: false, animated: true)
       
-      print("test")
+      print("testing")
       
       guard let main = tabBarController as? MainController else { return }
       
